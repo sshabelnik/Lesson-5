22651a90bd9b6902a184b82bfe4e4f4a71df57f4
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-10-16 16:19:38 +0300

    Initial Commit

---
+++ BlocksSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlocksSwift
+//
+//  Created by Ильдар Залялов on 16/10/2019.
+//  Copyright © 2019 Ildar Zalyalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
